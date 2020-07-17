388b4fa546198fd05066595bbba42c1ebd885a4c
gcairesdev <pessoalgcaires@gmail.com>
2020-07-17 20:31:55 -0300

    :sparkles: Added a collection point listing

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -25,6 +25,21 @@ class PointsController {
     return res.json({ id: point_id, ...point });
   }
 
+  async show(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const point = await knex('points').where('id', id).first();
+
+    if(!point) return res.status(400).json({ error: 'Point not found.' });
+
+    const items = await knex('items')
+      .join('point_items', 'items.id', '=', 'point_items.item_id')
+      .where('point_items.point_id', id)
+      .select('items.id', 'title', 'image');
+
+    return res.json({ ...point, items });
+  }
+
 }
 
 export default PointsController;
