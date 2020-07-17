4b87f93d695f3f7265860e2e71be9d508a2eec8c
gcairesdev <pessoalgcaires@gmail.com>
2020-07-17 14:23:51 -0300

    :art: Used routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  console.log('Server started');
-  res.send('Wello world');
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
