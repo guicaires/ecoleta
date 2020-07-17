efcf01127c01f7a0443c07ca6935d411fcab0c15
gcairesdev <pessoalgcaires@gmail.com>
2020-07-17 20:58:00 -0300

    :sparkles: Added a listing of a searched collection point

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -3,7 +3,25 @@ import knex from '../database/connection';
 
 class PointsController {
 
-  async create (req: Request, res: Response) {
+  async index(req: Request, res: Response) {
+    const { city, uf, items } = req.query;
+
+    const parsedItems = String(items)
+      .split(',')
+      .map(item => Number(item.trim()));
+
+    const points = await knex('points')
+      .join('point_items', 'points.id', '=', 'point_items.point_id')
+      .whereIn('point_items.item_id', parsedItems)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct()
+      .select('points.*');
+
+    return res.json(points);
+  }
+
+  async create(req: Request, res: Response) {
     const { name, email, whatsapp, longitude, latitude, city, uf, items } = req.body;
 
     const trx = await knex.transaction();
