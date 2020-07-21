66d287157984902faa697bee9a18afdbc87da130
gcairesdev <pessoalgcaires@gmail.com>
2020-07-21 15:56:48 -0300

    :sparkles: Added collection point registration function

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import axios from 'axios';
@@ -23,17 +23,18 @@ interface ibgeCityResponse {
 const CreatePoint = () => {
   const [items, setItems] = useState<Item[]>([]);
   const [cities, setCities] = useState<string[]>([]);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    whatsapp: ''
+  });
 
   const [selectedUF, setSelectedUF] = useState('0');
   const [selectedCity, setSelectedCity] = useState('0');
   const [selectedMarker, setSelectedMarker] = useState<[number, number]>([0,0]);
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
 
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    whatsapp: ''
-  });
+  const history = useHistory();
 
   useEffect(() => {
     api.get('items').then(res => {
@@ -86,6 +87,33 @@ const CreatePoint = () => {
     return setSelectedItems([...selectedItems, id]);
   }
 
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    const { name, email, whatsapp } = formData;
+    const uf = selectedUF;
+    const city = selectedCity;
+    const [latitude, longitude] = selectedMarker;
+    const items = selectedItems;
+
+    const data = {
+      name,
+      email,
+      whatsapp,
+      uf,
+      city,
+      latitude,
+      longitude,
+      items
+    };
+
+    await api.post('points', data);
+
+    alert('Ponto de coleta criado!');
+
+    history.push('/');
+  }
+
   return (
     <div id="page-create-point">
       <header>
@@ -97,7 +125,7 @@ const CreatePoint = () => {
         </Link>
       </header>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Cadastro do ponto de coleta</h1>
 
         <fieldset>
